10d761442b01426ccad8147164eb2944fb05ebaf
Vladislav <81467635+grGred@users.noreply.github.com>
2022-07-19 13:06:05 +0300

    use pragma ^0.8.4

--- contracts/BridgeBase.sol
+++ contracts/BridgeBase.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.4;
 
 import '@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol';
 import '@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol';
